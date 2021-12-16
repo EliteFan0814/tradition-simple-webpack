bd9cd63ac769a97ca90bcc7987640bb9fab64fae
fanpeichao <fan552426811@outlook.com>
2021-12-16 17:30:21 +0800

    fpc add

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 import './library/reset.css'
-import './index.html'
 import './css/index.scss'
+import './index.html'
 
-console.log($('.fpc').text())
+$(function () {
+  watchCloseDialog()
+  $('#verification-name').on('click', function (e) {
+    e.preventDefault()
+    console.log($('#search-select-value').val())
+  })
+  // 验证手机号
+  function isPhone(phone) {
+    var myreg = /^[1][3,4,5,7,8][0-9]{9}$/
+    if (!myreg.test(phone)) {
+      return false
+    } else {
+      return true
+    }
+  }
+  // 关闭弹框
+  function watchCloseDialog() {
+    $('.dialog-close').on('click', function () {
+      $('input[name="free-charge-value"]').val('')
+      $('input[name="free-charge-value-phone"]').val('')
+      $('input[name="universal-value-phone"]').val('')
+      $('input[name="oversea-value-phone"]').val('')
+      $('.free-charge').hide()
+      $('.universal-charge').hide()
+      $('.oversea-charge').hide()
+      $('.error').hide()
+      $('.mask').hide()
+    })
+  }
+})
