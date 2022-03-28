9a4f1f00a1a0057a470f0df7bb326dce8f1eab9a
fanpeichao <fan552426811@outlook.com>
2022-03-28 17:32:16 +0800

    fpc add

--- src/library/fpcHandle.js
+++ src/library/fpcHandle.js
@@ -21,7 +21,55 @@ function sss() {
     $('.form-dialog').show()
   })
 }
+// 快捷title点击
+function quickClick() {
+  $('.q-title').on('click', function (e) {
+    var quickindex = $(this).data('index')
+    $(this).addClass('q-title-active')
+    $('.quick-wrap').attr('class', 'quick-wrap quick' + quickindex)
+    $('.q-title').each(function (index, e) {
+      if (quickindex !== index) {
+        $(e).removeClass('q-title-active')
+      }
+    })
+  })
+}
+// 监听行业列表点击
+function watchIndustryListClick() {
+  $('.industry-list>li').on('click', function () {
+    var industryIndex = $(this).data('index')
+    if (industryIndex !== -1) {
+      $('.industry-main').show()
+      $('.industry-other').hide()
+      $('.core-item').each(function (index) {
+        if (industryIndex === index) {
+          $(this).css('display', 'flex')
+        } else {
+          $(this).css('display', 'none')
+        }
+      })
+      $('.relevance-item').each(function (index) {
+        if (industryIndex === index) {
+          $(this).css('display', 'flex')
+        } else {
+          $(this).css('display', 'none')
+        }
+      })
+    } else {
+      $('.industry-main').hide()
+      $('.industry-other').show()
+    }
+    $('.industry-list>li').each(function () {
+      if (industryIndex === $(this).data('index')) {
+        $(this).attr('class', 'active-li')
+      } else {
+        $(this).attr('class', '')
+      }
+    })
+  })
+}
 $(function () {
   watchCloseDialog()
-  sss()
+  quickClick()
+  watchIndustryListClick()
 })
