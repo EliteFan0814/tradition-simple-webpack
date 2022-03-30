578c2a799088170234d1f7680d3ed338dd72fa04
fanpeichao <fan552426811@outlook.com>
2022-03-30 16:37:09 +0800

    over

--- src/library/fpcHandle.js
+++ src/library/fpcHandle.js
@@ -81,10 +81,13 @@ function watchIndustryListClick() {
 }
 // 专业团队轮播图
 function newSwiperProfessor() {
-  var mySwiper = new Swiper('.swiper-professor', {
+  var swiperProfessor = new Swiper('.swiper-professor', {
     direction: 'horizontal', // 垂直切换选项
     loop: true, // 循环模式选项
-
+    speed: 300,
+    autoplay: {
+      delay: 3000
+    },
     // 如果需要分页器
     pagination: {
       el: '.professor-pagination',
@@ -95,21 +98,61 @@ function newSwiperProfessor() {
 }
 // 常见问题轮播图
 function newSwiperProblem() {
-  var mySwiper = new Swiper('.swiper-problem', {
+  var swiperProblem = new Swiper('.swiper-problem', {
     direction: 'horizontal', // 垂直切换选项
     loop: true, // 循环模式选项
-
+    speed: 300,
+    autoplay: {
+      delay: 3000
+    },
     // 如果需要分页器
     pagination: {
-      el: '.professor-pagination',
-      bulletClass: 'professor-unactive', // 非高亮class类
-      bulletActiveClass: 'professor-active' // 高亮class类
+      el: '.problem-pagination',
+      bulletClass: 'problem-unactive', // 非高亮class类
+      bulletActiveClass: 'problem-active' // 高亮class类
     }
+  })
+}
+// 推荐服务 商标交易轮播
+function newSwiperLogo() {
+  var swiperLogo = new Swiper('.swiper-logo', {
+    direction: 'horizontal', // 垂直切换选项
+    loop: true, // 循环模式选项
+    // 如果需要分页器
+    pagination: {
+      el: '.logo-pagination',
+      bulletClass: 'logo-unactive', // 非高亮class类
+      bulletActiveClass: 'logo-active' // 高亮class类
+    }
+  })
+}
+// 推荐服务title点击
+function serveClick() {
+  $('.s-tab-item').on('click', function (e) {
+    var serveIndex = $(this).data('index')
+    $(this).addClass('s-tab-active')
+    // $('.quick-wrap').attr('class', 'quick-wrap quick' + serveIndex)
+    $('.s-tab-item').each(function (index, e) {
+      if (serveIndex !== index) {
+        $(e).removeClass('s-tab-active')
+      }
+    })
+    $('.jq-serve-c').each(function () {
+      var serveContentIndex = $(this).data('index')
+      if (serveIndex === serveContentIndex) {
+        $(this).addClass('serve-c-active')
+      } else {
+        $(this).removeClass('serve-c-active')
+      }
+    })
   })
 }
 $(function () {
   newSwiperProfessor()
+  newSwiperProblem()
+  newSwiperLogo()
   watchCloseDialog()
   quickClick()
   watchIndustryListClick()
+  serveClick()
 })
